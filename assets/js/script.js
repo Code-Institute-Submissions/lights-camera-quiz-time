9fd69e0ee6c6ae226954c6f9fd16f54a87303689
louiseconlon11 <louiseconlon11@gmail.com>
2023-03-02 13:52:51 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function displayScifiQuestion() {
+
+}
+
+function displayComedyQuestion() {
+
+}
+
+function displayFantasyAdventureQuestion() {
+    
+}
