606b2634393bace4cbfbdc094f0c206329e99d0c
louiseconlon11 <louiseconlon11@gmail.com>
2023-03-02 14:06:27 +0000

    Add event listener for when each category is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+
+let scifi = document.getElementById("sci-fi");
+let score = document.getElementsByClassName("score");
+let question = document.getElementsByClassName("question-area");
+let answers = document.getElementsByClassName("answer-area");
+let timer = document.getElementsByClassName("timer");
+
+// Code adapted from the Love Maths Walkthrough for the function to execute when the DOM loads
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            let gameType = this.getAttribute("data-type")
+            if(gameType === "sci-fi") {
+                alert("You have chosen the scifi game")
+            } else [
+                alert(`You have chosen ${gameType}`)
+            ]
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -19,5 +42,5 @@ function displayComedyQuestion() {
 }
 
 function displayFantasyAdventureQuestion() {
-    
+
 }
