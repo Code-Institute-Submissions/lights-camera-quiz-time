4eafb00b8849dd2831b2da5fb29cb49f5ed2e8e4
louiseconlon11 <louiseconlon11@gmail.com>
2023-03-02 14:43:50 +0000

    Add instructions to display scifi question from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 
 let scifi = document.getElementById("sci-fi");
 let score = document.getElementsByClassName("score");
-let question = document.getElementsByClassName("question-area");
-let answers = document.getElementsByClassName("answer-area");
 let timer = document.getElementsByClassName("timer");
 
 // Code adapted from the Love Maths Walkthrough for the function to execute when the DOM loads
@@ -14,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
             let gameType = this.getAttribute("data-type")
             if(gameType === "sci-fi") {
                 alert("You have chosen the scifi game")
+                runGame();
             } else [
                 alert(`You have chosen ${gameType}`)
             ]
@@ -21,8 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-function runGame() {
 
+function runGame() {
+    if(gameType === "scifi") {
+        displayScifiQuestion();
+    }
 }
 
 function checkAnswer() {
@@ -34,7 +36,11 @@ function incrementScore() {
 }
 
 function displayScifiQuestion() {
-
+    let scifiQuestions = [{question: "Who is Luke Skywalkers' father?", Option1: "Han Solo", Option2: "Lando Calrissian", Option3: "Darth Vader", Option4: "Obi Wan", answer:"3"}]
+    let questionBox = document.getElementsByClassName("question-area");
+    let answerBox = document.getElementsByClassName("answer-area");
+    questionBox.textContent = scifiQuestions[0];
+    answerBox.textContent = scifiQuestions[1];
 }
 
 function displayComedyQuestion() {
